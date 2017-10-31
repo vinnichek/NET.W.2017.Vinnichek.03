bc77ef461340b0228f05a70cf6cb78d310f45836
ivinnichek <vinnichekira@gmail.com>
2017-10-31 21:15:11 +0300

    Add methods for three and four numbers

--- GreatestCommonDivisor.Logic/GreatestCommonDivisor.cs
+++ GreatestCommonDivisor.Logic/GreatestCommonDivisor.cs
@@ -9,7 +9,6 @@ namespace GreatestCommonDivisor.Logic
 {
     public class GreatestCommonDivisor
     {
-
         #region PublicMethods
 
         #region Public Euclid Methods
@@ -23,6 +22,34 @@ namespace GreatestCommonDivisor.Logic
         {
             int gcd = HelperForEuclidMethod(firstNumber, secondNumber);
             return gcd;
+        }
+
+        /// <summary>
+        /// Finds GCD using Euclid Method for three numbers.
+        /// </summary>
+        /// <param name="firstNumber">First int number. </param>
+        /// <param name="secondNumber">Second int number. </param>
+        /// <param name="thirdNumber">Second int number. </param>
+        /// <returns>GCD for three numbers. </returns>
+        public static int EuclidMethod(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int gcd = HelperForEuclidMethod(firstNumber, secondNumber);
+            return HelperForEuclidMethod(gcd, thirdNumber);
+        }
+
+        /// <summary>
+        /// Finds GCD using Euclid Method for four numbers.
+        /// </summary>
+        /// <param name="firstNumber">First int number. </param>
+        /// <param name="secondNumber">Second int number. </param>
+        /// <param name="thirdNumber">Second int number. </param>
+        /// <param name="fourthNumber">Second int number. </param>
+        /// <returns>GCD for four numbers. </returns>
+        public static int EuclidMethod(int firstNumber, int secondNumber, int thirdNumber, int foursNumber)
+        {
+            int gcd = HelperForEuclidMethod(firstNumber, secondNumber);
+            gcd = HelperForEuclidMethod(gcd, thirdNumber);
+            return HelperForEuclidMethod(gcd, foursNumber);
         }
 
         /// <summary>
@@ -40,7 +67,40 @@ namespace GreatestCommonDivisor.Logic
             }
             return gcd;
         }
-    #endregion
+
+        /// <summary>
+        /// Finds GCD using Euclid Method for two numbers.
+        /// </summary>
+        /// <param name="time">Method for time. </param>
+        /// <param name="firstNumber">First int number. </param>
+        /// <param name="secondNumber">Second int number. </param>
+        /// <returns>GCD for two numbers. </returns>
+        public static int EuclidMethod(out long time, int firstNumber, int secondNumber)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcd = EuclidMethod(firstNumber, secondNumber);
+            stopwatch.Stop();
+            time = stopwatch.ElapsedMilliseconds;
+            return gcd;
+        }
+
+        /// <summary>
+        /// Finds GCD using Euclid Method for two numbers.
+        /// </summary>
+        /// <param name="time">Method for time. </param>
+        /// <param name="array">Array of int numbers. </param>
+        /// <returns>GCD for two numbers. </returns>
+        public static int EuclidMethod(out long time, params int[] array)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcd = EuclidMethod(array);
+            stopwatch.Stop();
+            time = stopwatch.ElapsedMilliseconds;
+            return gcd;
+        }
+        #endregion
 
         #region Public Stein Methods
         /// <summary>
@@ -56,7 +116,35 @@ namespace GreatestCommonDivisor.Logic
         }
 
         /// <summary>
-        /// Finds GCD using Euclid Method for two numbers.
+        /// Finds GCD using Stein Method for three numbers.
+        /// </summary>
+        /// <param name="firstNumber">First int number. </param>
+        /// <param name="secondNumber">Second int number. </param>
+        /// <param name="thirdNumber">Second int number. </param>
+        /// <returns>GCD for two numbers. </returns>
+        public static int SteinMethod(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int gcd = HelperForEuclidMethod(firstNumber, secondNumber);
+            return HelperForEuclidMethod(gcd, thirdNumber);
+        }
+
+        /// <summary>
+        /// Finds GCD using Stein Method for four numbers.
+        /// </summary>
+        /// <param name="firstNumber">First int number. </param>
+        /// <param name="secondNumber">Second int number. </param>
+        /// <param name="thirdNumber">Second int number. </param>
+        /// <param name="foursNumber">Second int number. </param>
+        /// <returns>GCD for two numbers. </returns>
+        public static int SteinMethod(int firstNumber, int secondNumber, int thirdNumber, int fourthNumber)
+        {
+            int gcd = HelperForEuclidMethod(firstNumber, secondNumber);
+            gcd = HelperForEuclidMethod(gcd, thirdNumber);
+            return HelperForEuclidMethod(gcd, fourthNumber);
+        }
+
+        /// <summary>
+        /// Finds GCD using Stein Method for array of numbers.
         /// </summary>
         /// <param name="array">Array of int numbers. </param>
         /// <returns>GCD for all numbers from array. </returns>
@@ -70,14 +158,44 @@ namespace GreatestCommonDivisor.Logic
             }
             return gcd;
         }
-    #endregion
+
+        /// <summary>
+        /// Finds GCD using Stein Method for two numbers.
+        /// </summary>
+        /// <param name="time">Method for time. </param>
+        /// <param name="firstNumber">First int number. </param>
+        /// <param name="secondNumber">Second int number. </param>
+        /// <returns>GCD for two numbers. </returns>
+        private static int SteinMethod(out long time, int firstNumber, int secondNumber)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcd = SteinMethod(firstNumber, secondNumber);
+            stopwatch.Stop();
+            time = stopwatch.ElapsedMilliseconds;
+            return gcd;
+        }
+
+        /// <summary>
+        /// Finds GCD using Stein Method for two numbers.
+        /// </summary>
+        /// <param name="time">Method for time. </param>
+        /// <param name="array">Array of int numbers. </param>
+        /// <returns>GCD for array of int numbers. </returns>
+        private static int HelperForSteinMethod(out long time, params int[] array)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcd = SteinMethod(array);
+            stopwatch.Stop();
+            time = stopwatch.ElapsedMilliseconds;
+            return gcd;
+        }
+        #endregion
 
         #endregion
 
         #region Private Methods
-
-        #region Private Euclid Methods
-
         private static int HelperForEuclidMethod(int firstNumber, int secondNumber)
         {
             firstNumber = Math.Abs(firstNumber);
@@ -100,30 +218,6 @@ namespace GreatestCommonDivisor.Logic
             return firstNumber;
         }
 
-        private static int HelperForEuclidMethod(out long time, int firstNumber, int secondNumber)
-        {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            int gcd = EuclidMethod(firstNumber, secondNumber);
-            stopwatch.Stop();
-            time = stopwatch.ElapsedMilliseconds;
-
-            return gcd;
-        }
-
-        private static int HelperForEuclidMethod(out long time, params int[] array)
-        {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            int gcd = EuclidMethod(array);
-            stopwatch.Stop();
-            time = stopwatch.ElapsedMilliseconds;
-
-            return gcd;
-        }
-        #endregion
-
-        #region Private Stein Methods
         private static int HelperForSteinMethod(int firstNumber, int secondNumber)
         {
             firstNumber = Math.Abs(firstNumber);
@@ -152,29 +246,6 @@ namespace GreatestCommonDivisor.Logic
             return gcd;
         }
 
-        private static int HelperForSteinMethod(out long time, int firstNumber, int secondNumber)
-        {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            int gcd = SteinMethod(firstNumber, secondNumber);
-            stopwatch.Stop();
-            time = stopwatch.ElapsedMilliseconds;
-
-            return gcd;
-        }
-
-        private static int HelperForSteinMethod(out long time, params int[] array)
-        {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-            int gcd = SteinMethod(array);
-            stopwatch.Stop();
-            time = stopwatch.ElapsedMilliseconds;
-
-            return gcd;
-        }
-        #endregion
-
         private static void ArrayChecker(int[] array)
         {
             if (array == null)
@@ -192,7 +263,6 @@ namespace GreatestCommonDivisor.Logic
                 throw new ArgumentException("Need at least two elements.");
             }
         }
-
         #endregion
     }
 }
